4bcae3a10cc56faf18d206c359ce3d943b146191
fabianRafreider <fabian@rafreider.com>
2023-05-20 22:31:00 +0200

    Initial commit

---
+++ src/wifiSniffer/wifiSniffer.cpp
@@ -0,0 +1,72 @@
+#include "wifiSniffer.h"
+
+WifiSniffer::WifiSniffer(){
+    minWifiChannel = 1;
+    maxWifiChannel =  13;
+
+
+
+    wifiSnifferPacketHandler = [this](void* buff, wifi_promiscuous_pkt_type_t type) {
+    if (type != WIFI_PKT_MGMT) //check if packet is management packet -> only management packets contain SSID
+        return;
+    const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
+    const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
+    const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr; //get the mac address of the sender of the packet (the access point)
+
+    char mac_add[35];
+    sprintf(mac_add,"%02x:%02x:%02x:%02x:%02x:%02x", hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],hdr->addr2[3],hdr->addr2[4],hdr->addr2[5]);
+
+    bool found = false;
+
+    for (auto mac = macAdresses.begin(); mac != macAdresses.end();){ // go throu the list of mac adresses
+        if (mac->macAdress == mac_add){ // check if the mac adress is already in the list
+            mac->timestamp = millis(); // update the timestamp
+            found = true;
+        }
+        else if (mac->timestamp + 600000 < millis()){ // check if the mac adress is older than 10 minutes
+            this->macAdresses.erase(mac); // remove the mac adress from the list
+        }
+        else{
+            mac++;
+        }
+    }
+};
+}
+
+
+WifiSniffer::WifiSniffer(uint8_t minChannel, uint8_t maxChannel){
+    minWifiChannel = minChannel;
+    maxWifiChannel =  maxChannel;
+}
+
+WifiSniffer::~WifiSniffer(){
+    //Destructor
+}
+
+void WifiSniffer::init(){
+    wifi_country_t wifi_country = { //To use powerlimits in Germany
+        .cc = "DE",
+        .schan = minWifiChannel,
+        .nchan = maxWifiChannel,
+        .max_tx_power = 100,
+    };
+    //Init
+    tcpip_adapter_init();
+    // esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    esp_wifi_set_country(&wifi_country); /* set country for channel range [1, 13] */
+    esp_wifi_set_storage(WIFI_STORAGE_RAM);
+    esp_wifi_set_mode(WIFI_MODE_NULL);
+    esp_wifi_start();
+    esp_wifi_set_promiscuous(true);
+    auto cb = reinterpret_cast<wifi_promiscuous_cb_t>(wifiSnifferPacketHandler.target<void(void*, wifi_promiscuous_pkt_type_t)>()); //Convert lambda to function pointer
+    esp_wifi_set_promiscuous_rx_cb(cb);
+}
+
+void WifiSniffer::setChannel(uint8_t & channel){
+    esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+    channel += 1;
+    if (channel > maxWifiChannel) 
+        channel = minWifiChannel;
+}
---
+++ src/wifiSniffer/wifiSniffer.h
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <vector>
+#include <functional>
+
+// inspiration from: https://github.com/DiJei/esp32-wifi-sniffer/tree/main
+
+class WifiSniffer{
+    private:  
+
+        typedef struct { // wifi packet header (when promiscuous mode is enabled)
+            unsigned frame_ctrl:16;
+            unsigned duration_id:16;
+            uint8_t addr1[6]; /* receiver address */
+            uint8_t addr2[6]; /* sender address */
+            uint8_t addr3[6]; /* filtering address */
+            unsigned sequence_ctrl:16;
+            uint8_t addr4[6]; /* optional */
+        } wifi_ieee80211_mac_hdr_t;
+
+
+        typedef struct { // wifi packet header (when promiscuous mode is enabled)
+            wifi_ieee80211_mac_hdr_t hdr;
+            uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
+        } wifi_ieee80211_packet_t;
+
+        typedef struct { // the format the mac adress is saved in
+            uint32_t timestamp;
+            const char macAdress [35];
+        }macAdress;
+
+        std::vector<macAdress> macAdresses; // vector to save the mac adresses
+
+        uint8_t minWifiChannel;
+        uint8_t maxWifiChannel;
+
+        std::function<void(void*, wifi_promiscuous_pkt_type_t)> wifiSnifferPacketHandler;
+
+        // static void wifiSnifferPacketHandler(void* buff, wifi_promiscuous_pkt_type_t type); // set as static to be able to use it as callback
+    public:
+        WifiSniffer();
+        WifiSniffer(uint8_t minChannel, uint8_t maxChannel);
+        ~WifiSniffer();
+        void init();
+        void setChannel(uint8_t & channel);
+};
