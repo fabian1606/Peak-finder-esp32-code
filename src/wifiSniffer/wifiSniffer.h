335f70ade6420363acf629716ec1638b5613bf4c
fabianRafreider <fabian@rafreider.com>
2023-05-21 09:23:10 +0200

    feat: added prerelease function

--- src/wifiSniffer/wifiSniffer.h
+++ src/wifiSniffer/wifiSniffer.h
@@ -1,48 +1,53 @@
-// #include <Arduino.h>
-// #include <WiFi.h>
-// #include <esp_wifi.h>
-// #include <vector>
-// #include <functional>
-// #include <iostream>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <vector>
+#include <functional>
+#include <iostream>
 
-// // inspiration from: https://github.com/DiJei/esp32-wifi-sniffer/tree/main
+// inspiration from: https://github.com/DiJei/esp32-wifi-sniffer/tree/main
 
-// class WifiSniffer{
-//     private:  
+class WifiSniffer{
+    private:  
+        WifiSniffer();
+        WifiSniffer(uint8_t minChannel, uint8_t maxChannel);
+        ~WifiSniffer();
 
-//         typedef struct { // wifi packet header (when promiscuous mode is enabled)
-//             unsigned frame_ctrl:16;
-//             unsigned duration_id:16;
-//             uint8_t addr1[6]; /* receiver address */
-//             uint8_t addr2[6]; /* sender address */
-//             uint8_t addr3[6]; /* filtering address */
-//             unsigned sequence_ctrl:16;
-//             uint8_t addr4[6]; /* optional */
-//         } wifi_ieee80211_mac_hdr_t;
+        typedef struct { // wifi packet header (when promiscuous mode is enabled)
+            unsigned frame_ctrl:16;
+            unsigned duration_id:16;
+            uint8_t addr1[6]; /* receiver address */
+            uint8_t addr2[6]; /* sender address */
+            uint8_t addr3[6]; /* filtering address */
+            unsigned sequence_ctrl:16;
+            uint8_t addr4[6]; /* optional */
+        } wifi_ieee80211_mac_hdr_t;
 
 
-//         typedef struct { // wifi packet header (when promiscuous mode is enabled)
-//             wifi_ieee80211_mac_hdr_t hdr;
-//             uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
-//         } wifi_ieee80211_packet_t;
+        typedef struct { // wifi packet header (when promiscuous mode is enabled)
+            wifi_ieee80211_mac_hdr_t hdr;
+            uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
+        } wifi_ieee80211_packet_t;
 
-//         typedef struct { // the format the mac adress is saved in
-//             uint32_t timestamp;
-//             const char macAdress [35];
-//         }macAdress;
 
-//         std::vector<macAdress> macAdresses; // vector to save the mac adresses
 
-//         uint8_t minWifiChannel;
-//         uint8_t maxWifiChannel;
+        uint8_t minWifiChannel;
+        uint8_t maxWifiChannel;
 
-//         std::function<void(void*, wifi_promiscuous_pkt_type_t)> wifiSnifferPacketHandler;
+        static void wifiSnifferPacketHandler (void* buff, wifi_promiscuous_pkt_type_t type); //set as static because it is called from a c function
 
-//         // static void wifiSnifferPacketHandler(void* buff, wifi_promiscuous_pkt_type_t type); // set as static to be able to use it as callback
-//     public:
-//         WifiSniffer();
-//         WifiSniffer(uint8_t minChannel, uint8_t maxChannel);
-//         ~WifiSniffer();
-//         void init();
-//         void setChannel(uint8_t & channel);
-// };
+    public:
+        static WifiSniffer& getInstance() { // singleton pattern to ensure that only one instance of the class exists (i cant use the wifi instance in multiple objects)
+            static WifiSniffer instance;
+            return instance;
+        }
+
+        typedef struct { // the format the mac adress is saved in
+            uint32_t timestamp;
+            const char macAdress [35];
+        }macAdress;
+
+        std::vector<macAdress>macAdresses; // vector to save the mac adresses
+        void init();
+        void setChannel(uint8_t & channel);
+};
